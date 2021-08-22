5c06d0259bc47447ffb6caa635cffbce3559e4e5
lautaroilla <lautaro12387@gmail.com>
2021-08-22 17:50:30 -0300

    WorkSpace

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function verificar(usuario, contraseña){
+    let user = {};
+    if(usuario.trim()===""){
+        alert("Indica tu nombre de usuario") ;  
+    } else if (contraseña.trim()==="")
+    alert("Coloca correctamente tu contraseña")
+    else{
+        user.nombre = usuario;
+        user.contraseña = contraseña;
+        user.estado = "conectado";
+        localStorage.setItem("usuario",JSON.stringify(user));
+        location.href="index.html";
+    }
+    }
+    
+    function desconectar() {
+    localStorage.clear();
+    }
 
-});
+    document.addEventListener("DOMContentLoaded", function(e){
+
+    });
